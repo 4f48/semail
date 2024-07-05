7cd93d043c71286c1ce8906ccbce2d89d07ff370
4f48 <0x4f48@proton.me>
2024-07-05 13:14:50 +0200

    Move DB code to separate module

---
+++ src/db.rs
@@ -0,0 +1,28 @@
+use sea_orm::{ConnectOptions, Database, DatabaseConnection, DbErr};
+use sqlx::migrate::MigrateDatabase;
+use sqlx::Sqlite;
+use std::time::Duration;
+
+const DB_URL: &str = "sqlite://sqlite.db";
+pub async fn create_db() {
+    if !Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
+        println!("Creating database {}", DB_URL);
+        match Sqlite::create_database(DB_URL).await {
+            Ok(_) => println!("Successfully created database"),
+            Err(error) => panic!("Something went wrong while creating database: {}", error),
+        }
+    }
+}
+
+pub async fn connect_db() -> Result<DatabaseConnection, DbErr> {
+    let mut opt = ConnectOptions::new("sqlite://sqlite.db");
+    opt.max_connections(100)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(8))
+        .acquire_timeout(Duration::from_secs(8))
+        .idle_timeout(Duration::from_secs(8))
+        .max_lifetime(Duration::from_secs(8))
+        .sqlx_logging(true)
+        .sqlx_logging_level(log::LevelFilter::Info);
+    Database::connect(opt).await
+}
