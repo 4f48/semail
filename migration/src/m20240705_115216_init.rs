7e6616745871479681bb887278131b1c1e013b2f
4f48 <0x4f48@proton.me>
2024-07-05 14:40:30 +0200

    Finish tables in up and down functions

--- migration/src/m20240705_115216_init.rs
+++ migration/src/m20240705_115216_init.rs
@@ -8,7 +8,8 @@ enum Accounts {
     Table,
     Id,
     Name,
-    Mail,
+    PublicKey,
+    PrivateKey,
     Password,
 }
 
@@ -30,37 +31,64 @@ impl MigrationTrait for Migration {
                 Table::create()
                     .table(Accounts::Table)
                     .if_not_exists()
-                    .col(ColumnDef::new(Accounts::Id).primary_key().uuid().not_null())
+                    .col(
+                        ColumnDef::new(Accounts::Id)
+                            .primary_key()
+                            .uuid()
+                            .unique_key()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(Accounts::Name)
+                            .string()
+                            .unique_key()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(Accounts::PublicKey)
+                            .string()
+                            .unique_key()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(Accounts::PrivateKey)
+                            .string()
+                            .unique_key()
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(Accounts::Password).string().not_null())
                     .to_owned(),
             )
-            .await
-
-        /*manager
-        .create_table(
-            Table::create()
-                .table(Post::Table)
-                .if_not_exists()
-                .col(
-                    ColumnDef::new(Post::Id)
-                        .integer()
-                        .not_null()
-                        .auto_increment()
-                        .primary_key(),
-                )
-                .col(ColumnDef::new(Post::Title).string().not_null())
-                .col(ColumnDef::new(Post::Text).string().not_null())
-                .to_owned(),
-        )
-        .await*/
+            .await?;
+        
+        manager
+            .create_table(
+                Table::create()
+                    .table(Mails::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Mails::Id)
+                            .primary_key()
+                            .uuid()
+                            .unique_key()
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(Mails::From).string().not_null())
+                    .col(ColumnDef::new(Mails::To).string().not_null())
+                    .col(ColumnDef::new(Mails::Subject).string())
+                    .col(ColumnDef::new(Mails::Body).string())
+                    .to_owned(),
+            )
+            .await?;
+        
+        Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
+        manager.drop_table(Table::drop().table(Accounts::Table).to_owned()).await?;
+        manager.drop_table(Table::drop().table(Mails::Table).to_owned()).await?;
 
-        manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
-            .await
+        Ok(())
     }
 }
 
