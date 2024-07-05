934b46bf291eab9251df9c24d6199ff8e1211118
4f48 <0x4f48@proton.me>
2024-07-05 20:01:04 +0200

    Fixed double double quotes in emails

--- src/routes/receive.rs
+++ src/routes/receive.rs
@@ -1,32 +1,43 @@
 use crate::db;
-use crate::db::connect_db;
 use axum::{Json};
 use entity::mails::ActiveModel;
 use sea_orm::ActiveValue::Set;
 use sea_orm::{ActiveModelTrait, DbErr};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 use serde_json::Value;
 use uuid::{uuid, Uuid};
 
+#[derive(Serialize, Deserialize)]
+struct Payload {
+    from: String,
+    to: String,
+    subject: String,
+    body: String,
+}
+
 pub async fn main(Json(payload): Json<Value>) -> Json<Value> {
-    if payload["from"].is_null() || payload["to"].is_null() {
-        return Json(json!({
+    let payload: Payload = match serde_json::from_value(payload) {
+        Ok(payload) => payload,
+        Err(error) => {
+            return Json(json!({
             "error": "mail wasn't forwarded to recipient",
-            "reason": "the 'from' and or 'to' value(s) are missing from json body"
+            "reason": "the 'from' and or 'to' value(s) are missing from json body",
+            "detailed_error": format!("{}", error),
         }))
-    }
+        }
+    };
     
-    // for some reason double quotes get included in strings too: FIX LATER
     match insert(
-        payload["from"].to_string(),
-        payload["to"].to_string(),
-        Option::from(payload["subject"].to_string()),
-        Option::from(payload["body"].to_string()),
+        payload.from,
+        payload.to,
+        Option::from(payload.subject),
+        Option::from(payload.body),
     )
     .await
     {
         Ok(_) => Json(json!({
-            "success": format!("email forwarded to recipient: {}", payload["to"]),
+            "success": format!("email forwarded to recipient"),
         })),
         Err(error) => Json(json!({
             "error": format!("{:?}", error),
@@ -43,7 +54,7 @@ async fn insert(
     let db = db::connect_db().await?;
     let mail = ActiveModel {
         id: Set(Uuid::now_v7()),
-        owner: Set(uuid!("019083d2-86c7-7d22-947d-b4c3937db73b")), // generate random for now, query user from username and instance IP/domain in the future
+        owner: Set(uuid!("019083d2-86c7-7d22-947d-b4c3937db73b")), // hard coded for now, query uuid from username and instance IP/domain in the future
         from: Set(from),
         to: Set(to),
         subject: Set(subject),
