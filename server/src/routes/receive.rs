4274bd422db8d0141184e2360643a6c382a2241b
4f48 <0x4f48@proton.me>
2024-07-07 16:38:59 +0200

    uuid_from_recipient function progress

--- server/src/routes/receive.rs
+++ server/src/routes/receive.rs
@@ -7,6 +7,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::json;
 use serde_json::Value;
 use uuid::{uuid, Uuid};
+use regex::Regex;
 
 #[derive(Serialize, Deserialize)]
 struct Payload {
@@ -56,13 +57,48 @@ async fn insert(
         id: Set(Uuid::now_v7()),
         owner: Set(uuid!("019083d2-86c7-7d22-947d-b4c3937db73b")), // hard coded for now, query uuid from username and instance IP/domain in the future
         from: Set(from),
-        to: Set(to),
+        to: Set(to.clone()),
         subject: Set(subject),
         body: Set(body),
         ..Default::default()
     };
 
     mail.insert(&db).await?;
+    
+    uuid_from_recipient(to).await.unwrap();
 
     Ok(())
+}
+
+#[derive(Debug)]
+enum Error {
+    AddressFormatError,
+    WrongInstance,
+}
+
+#[derive(Debug)]
+struct Recipient {
+    account: String,
+    instance: String,
+}
+
+async fn uuid_from_recipient(recipient: String) -> Result<Uuid, Error> {
+    let regex = Regex::new(r"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").unwrap();
+    if !regex.is_match(&*recipient) {
+        return Err(Error::AddressFormatError);
+    };
+    
+    let recipient = recipient.split("@").collect::<Vec<&str>>();
+    let recipient = Recipient {
+        account: recipient[0].parse().unwrap(),
+        instance: recipient[1].parse().unwrap()
+    };
+    
+    if recipient.instance != "example.dev" {
+        return Err(Error::WrongInstance)
+    }
+    
+    // query db for uuid
+
+    Ok(uuid!("019083d2-86c7-7d22-947d-b4c3937db73b"))
 }
