65070d979dc4824d072206562312b6c773ea1540
4f48 <0x4f48@proton.me>
2024-07-08 16:23:36 +0200

    Do it with a bit of JS instead
    This will be able to capture the response better. Not pure HTML, but that would be impossible anyway.

--- server/src/routes/auth/register.rs
+++ server/src/routes/auth/register.rs
@@ -1,6 +1,7 @@
 use axum::http::StatusCode;
 use axum::{Form, Json};
 use axum::response::Redirect;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 
@@ -11,5 +12,15 @@ pub struct FormData {
 }
 
 pub async fn main(Form(form_data): Form<FormData>) -> Redirect {
+    let regex = Regex::new(r"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").unwrap();
+    if !regex.is_match(&form_data.address) {
+        return (
+            StatusCode::BAD_REQUEST,
+            Json(json!({
+                "error": "malformed se-mail address"
+            })),
+        );
+    };
+    
     Redirect::to("http://localhost:4321/")
 }
