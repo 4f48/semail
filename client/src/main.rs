dd93456fed64eba93383840eb233b44b89ff10b0
4f48 <0x4f48@proton.me>
2024-07-11 17:10:51 +0200

    Use dioxus instead

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,14 +1,51 @@
-use client::App;
-use leptos::*;
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use tracing::Level;
+
+#[derive(Clone, Routable, Debug, PartialEq)]
+enum Route {
+    #[route("/")]
+    Home {},
+    #[route("/blog/:id")]
+    Blog { id: i32 },
+}
 
 fn main() {
-    // set up logging
-    _ = console_log::init_with_level(log::Level::Debug);
-    console_error_panic_hook::set_once();
+    // Init logger
+    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    launch(App);
+}
 
-    mount_to_body(|| {
-        view! {
-            <App />
+fn App() -> Element {
+    rsx! {
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Blog(id: i32) -> Element {
+    rsx! {
+        Link { to: Route::Home {}, "Go to counter" }
+        "Blog post {id}"
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    let mut count = use_signal(|| 0);
+
+    rsx! {
+        Link {
+            to: Route::Blog {
+                id: count()
+            },
+            "Go to blog"
         }
-    })
+        div {
+            h1 { "High-Five counter: {count}" }
+            button { onclick: move |_| count += 1, "Up high!" }
+            button { onclick: move |_| count -= 1, "Down low!" }
+        }
+    }
 }
