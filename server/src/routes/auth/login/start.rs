ad2755361ee52c3fcea38485dfe87a6387fefc93
4f48 <0x4f48@proton.me>
2024-07-12 22:34:32 +0200

    Add copyright and license notices to every major file

--- server/src/routes/auth/login/start.rs
+++ server/src/routes/auth/login/start.rs
@@ -1,3 +1,21 @@
+/*
+ * This file is part of SE-Mail.
+ *
+ * Copyright © 2024 Oliver Pirger <0x4f48@proton.me>
+ *
+ * SE-Mail is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License, version 3, as published by the Free Software Foundation.
+ *
+ * SE-Mail is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License along with SE-Mail.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
 use crate::common::db;
 use crate::common::opaque::Default as DefaultSuite;
 use crate::AppState;
--- server/src/routes/receive.rs
+++ server/src/routes/receive.rs
@@ -1,3 +1,21 @@
+/*
+ * This file is part of SE-Mail.
+ *
+ * Copyright © 2024 Oliver Pirger <0x4f48@proton.me>
+ *
+ * SE-Mail is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License, version 3, as published by the Free Software Foundation.
+ *
+ * SE-Mail is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License along with SE-Mail.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
 use crate::common::db;
 use axum::http::StatusCode;
 use axum::Json;
@@ -14,7 +32,7 @@ use serde_json::Value;
 use std::env;
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 struct Payload {
     from: String,
     to: String,
